801981dda782802d1f2b5e1fd09efcb84d648b2f
duetooyou <60111205+duetooyou@users.noreply.github.com>
2021-11-16 12:46:38 +0500

    Update urls.py

--- clicksber_test/urls.py
+++ clicksber_test/urls.py
@@ -5,7 +5,7 @@ from crm.views import home_page
 
 
 urlpatterns = [
-    path('', home_page, name='home'),
+    path('bday_vlada', home_page, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
